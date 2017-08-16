47da354208c6cd3dffeebb5a76d7d9fede840a0f
SydraCZ <SydneyChen2011@gmail.com>
2017-08-17 00:52:38 +0800

    Replace conditional with polymorphism(255)

--- src/cyd/refactor/Movie.java
+++ src/cyd/refactor/Movie.java
@@ -11,19 +11,33 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String _title;        // 名稱
-    private int _priceCode;       // 價格
+    private Price _priceCode;       // 價格
 
     public Movie(String title, int priceCode){
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _priceCode.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch(arg){   // 取得影片出租價格
+            case Movie.REGULAR:                     // 普通片
+                _priceCode = new RegularPrice();
+                break;
+
+            case Movie.NEW_RELEASE:         // 新片
+                _priceCode = new NewReleasePrice();
+                break;
+
+            case Movie.CHILDREN:           // 兒童片
+                _priceCode = new ChildrenPrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String getTitle() {
@@ -32,32 +46,60 @@ public class Movie {
 
 
     public double getCharge(int daysRented) {
-        double result = 0;
-        switch(_priceCode){   // 取得影片出租價格
-            case Movie.REGULAR:                     // 普通片
-                result += 2;
-                if(daysRented>2)
-                    result += (daysRented-2)*1.5;
-                break;
+        return _priceCode.getCharge(daysRented);
+    }
 
-            case Movie.NEW_RELEASE:         // 新片
-                result += daysRented*3;
-                break;
+    public int getFrequentRenterPoints(int daysRented){
+        _priceCode.getFrequentRenterPoints(daysRented);
+    }
 
-            case Movie.CHILDREN:           // 兒童片
-                result += 1.5;
-                if(daysRented>3)
-                    result += (daysRented-3)*1.5;
-                break;
-        }
+}
+
+
+/**
+ * Created by sydneychen on 17/8/17.
+ */
+abstract class Price {
+
+    abstract int getPriceCode();
+    abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented){
+        return 1;
+    }
+}
+class ChildrenPrice extends Price {
+    int getPriceCode() {
+        return Movie.CHILDREN;
+    }
+    double getCharge(int daysRented) {
+        double result = 1.5;
+        if(daysRented>3)
+            result += (daysRented-3)*1.5;
         return result;
     }
-    public int getFrequentRenterPoints(int daysRented){
+}
+class RegularPrice extends Price {
+    int getPriceCode() {
+        return Movie.REGULAR;
+    }
+    double getCharge(int daysRented) {
+        double result = 2;
+        if(daysRented>2)
+            result += (daysRented-2)*1.5;
+        return result;
+    }
+}
+class NewReleasePrice extends Price {
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+    double getCharge(int daysRented) {
+        return daysRented*3;
+    }
+    int getFrequentRenterPoints(int daysRented){
         // add bonus for a two day new release rental
-        if ((_priceCode == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-        else
-            return 1;
+        return daysRented > 1?2:1;
     }
 }
 
